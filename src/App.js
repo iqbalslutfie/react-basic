a07a047728c4d2dbcc59ae6a09b1d146eafcf935
iqbal Syarifudin lutfie <iqbal.lutfie.il@gmail.com>
2021-01-17 10:00:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
-import React, { Component } from 'react'
-import './App.css';
-import Count from './components/Count'
-import Axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import Count from "./components/Count";
+import Axios from "axios";
 
 export class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      count : 0,
-      data :[]
-    }
+      count: 0,
+      data: [],
+    };
   }
 
   clickCountIncrease = () => {
     this.setState({
-      count : this.state.count + 1
-    })
-  }
+      count: this.state.count + 1,
+    });
+  };
 
   clickCountDecrease = () => {
     this.setState({
-      count : this.state.count - 1 
-    })
-  }
+      count: this.state.count - 1,
+    });
+  };
 
   componentDidMount() {
-    Axios.get('http://localhost:9090/api/interview/list/view?filter=wawancara_id=1&orderby=&top=&skip=&parent_code=').then( res =>
+    Axios.get(
+      "http://localhost:9090/api/interview/list/view?filter=wawancara_id=1&orderby=&top=&skip=&parent_code="
+    ).then((res) =>
       this.setState({
-        data : res.data.items
+        data: res.data.items,
       })
-    )
+    );
   }
 
   render() {
     return (
       <div className="App">
-        {/* <h1>Click Count : {this.state.count}</h1>
-        <Count increase={this.clickCountIncrease} decrease = {this.clickCountDecrease}/> */}
-        {this.state.data.map((data, x) => 
+        <div>
+          <h1>Click Count : {this.state.count}</h1>
+          <Count
+            increase={this.clickCountIncrease}
+            decrease={this.clickCountDecrease}
+          />
+        </div>
+        {this.state.data.map((data, x) => (
           <div key={x}>
             <p>Wawancara Id : {data.wawwancaraId}</p>
             <p>Pertanyaan : {data.pertanyaan}</p>
           </div>
-        )}
+        ))}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
